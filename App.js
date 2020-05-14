6366bfb8fc3b61104823c227651f60f1423491d0
Najih Mld <najihmaulida@gmail.com>
2020-05-14 20:56:03 +0700

    add multiple stack navigator

--- App.js
+++ App.js
@@ -2,16 +2,35 @@ import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
+import Main from './src/app/Main';
 import Home from './src/app/Home';
 import Repo from './src/app/Repo';
 
 const Stack = createStackNavigator();
 
-class App extends Component {
+class MainScreen extends Component {
+  render() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          options={{headerShown: false}}
+          component={Main}
+        />
+      </Stack.Navigator>
+    );
+  }
+}
+export default class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Main" component={MainScreen} />
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Repo" component={Repo} />
         </Stack.Navigator>
@@ -19,5 +38,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
---
+++ src/app/Main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text, Button} from 'react-native';
+
+class SplashScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>SplashScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default SplashScreen;
