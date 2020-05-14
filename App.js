6d2cbd9fc16417c5786f89fd8952a2acc692b4d3
Najih Mld <najihmaulida@gmail.com>
2020-05-14 23:10:01 +0700

    update stack navigator

--- App.js
+++ App.js
@@ -3,25 +3,11 @@ import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import Main from './src/app/Main';
 import Home from './src/app/Home';
 import Repo from './src/app/Repo';
 
 const Stack = createStackNavigator();
 
-class MainScreen extends Component {
-  render() {
-    return (
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Main"
-          options={{headerShown: false}}
-          component={Main}
-        />
-      </Stack.Navigator>
-    );
-  }
-}
 export default class App extends Component {
   render() {
     return (
@@ -30,7 +16,6 @@ export default class App extends Component {
           screenOptions={{
             headerShown: false,
           }}>
-          <Stack.Screen name="Main" component={MainScreen} />
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Repo" component={Repo} />
         </Stack.Navigator>
